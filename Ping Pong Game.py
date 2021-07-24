4349d2fce1b604075735e92b7ce38ff533179ec2
TheStorm64 <87566761+TheStorm64@users.noreply.github.com>
2021-07-24 11:32:40 +0100

    Add files via upload

---
+++ Ping Pong Game.py
@@ -0,0 +1,26 @@
+from pygame import *
+
+class GameSprite(sprite.Sprite):
+ #class constructor
+   def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+       #Call for the class (Sprite) constructor:
+       sprite.Sprite.__init__(self)
+ 
+       #every sprite must store the image property
+       self.image = transform.scale(image.load(player_image), (size_x, size_y))
+       self.speed = player_speed
+ 
+       #every sprite must have the rect property – the rectangle it is fitted in
+       self.rect = self.image.get_rect()
+       self.rect.x = player_x
+       self.rect.y = player_y
+ #method drawing the character on the window
+   def reset(self):
+       window.blit(self.image, (self.rect.x, self.rect.y))
+
+win_width = 700
+win_height = 500
+display.set_caption("Ping Pong")
+window = display.set_mode((win_width, win_height))
+background = transform.scale(image.load(img_back), (win_width, win_height))
+
